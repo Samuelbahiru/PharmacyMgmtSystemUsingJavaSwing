80d3a9b8f10a73c474c931649e9ec20284bc3cdd
samuelbahiru <samuelbahiru93@gmail.com>
2024-05-23 16:05:33 +0300

    adding home and register page

---
+++ src/pams/PAMS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pams;
+
+/**
+ *
+ * @author sam
+ */
+public class PAMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HomePage obj = new HomePage();
+        obj.show();
+    }
+    
+}
